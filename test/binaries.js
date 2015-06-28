9dc23604e97dffdb1dc32dfdc885f9382c1d11ce
Cristian Greco <cristianrgreco@gmail.com>
2015-06-28 14:33:41 +0100

    Binaries test cases refactored to remove setUp.

--- test/binaries.js
+++ test/binaries.js
@@ -1,30 +1,37 @@
 'use strict';
 
 let testCase = require('nodeunit').testCase;
+let binaries = require('../src/binaries');
 
 module.exports = testCase({
-    'setUp': callback => {
-        delete require.cache[require.resolve('../src/binaries')];
-        this.binaries = require('../src/binaries');
-        callback();
-    },
-
     'allValid': test => {
-        this.binaries.valid().then(() => test.done());
+        binaries.valid().then(() => test.done());
     },
 
     'isInvalidYouTubeDl': test => {
-        this.binaries.paths.youtubeDl = this.binaries.paths.ffmpeg;
-        this.binaries.valid().then(null, () => test.done());
+        let defaultYoutubeDlPath = binaries.paths.youtubeDl;
+        binaries.paths.youtubeDl = binaries.paths.ffmpeg;
+        binaries.valid().then(null, () => {
+            binaries.paths.youtubeDl = defaultYoutubeDlPath;
+            test.done()
+        });
     },
 
     'isInvalidFfmpeg': test => {
-        this.binaries.paths.ffmpeg = this.binaries.paths.youtubeDl;
-        this.binaries.valid().then(null, () => test.done());
+        let defaultFfmpegPath = binaries.paths.ffmpeg;
+        binaries.paths.ffmpeg = binaries.paths.youtubeDl;
+        binaries.valid().then(null, () => {
+            binaries.paths.ffmpeg = defaultFfmpegPath;
+            test.done()
+        });
     },
 
     'isInvalidFfprobe': test => {
-        this.binaries.paths.ffprobe = this.binaries.paths.youtubeDl;
-        this.binaries.valid().then(null, () => test.done());
+        let defaultFfprobePath = binaries.paths.ffprobe;
+        binaries.paths.ffprobe = binaries.paths.youtubeDl;
+        binaries.valid().then(null, () => {
+            binaries.paths.ffprobe = defaultFfprobePath;
+            test.done()
+        });
     }
 });
